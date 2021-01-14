3e4528a044c1705bdea47f48a6577a74048a6331
philiplee25 <yplunap25@gmail.com>
2021-01-14 18:36:59 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -3,30 +3,42 @@ package com.eomcs.pms;
 public class App {
 
   public static void main(String[] args) {
+    System.out.println("[회원]");
+
+
     java.io.InputStream keyboard = System.in;
     java.util.Scanner keyScan = new java.util.Scanner(keyboard);
 
-    System.out.println("[회원]");
     System.out.print("번호? ");
     String id = keyScan.nextLine();
+
     System.out.print("이름? ");
     String name = keyScan.nextLine();
+
     System.out.print("이메일? ");
     String eMail = keyScan.nextLine();
+
     System.out.print("암호? ");
     String pw = keyScan.nextLine();
+
     System.out.print("사진? ");
     String pic = keyScan.nextLine();
+
     System.out.print("전화? ");
-    String phone = keyScan.nextLine();
+    String tel = keyScan.nextLine();
+
+    java.sql.Date now = new java.sql.Date(System.currentTimeMillis());
+
+    keyScan.close();
+
     System.out.println("-------------------------------------");
     System.out.printf("번호: %s\n", id);
     System.out.printf("이름: %s\n", name);
-    System.out.printf("이메일: %s\n", eMail);
+    System.out.println("이메일:"+eMail);
     System.out.printf("암호: %s\n", pw);
     System.out.printf("사진: %s\n", pic);
-    System.out.printf("전화: %s\n", phone);
-
+    System.out.printf("전화: %s\n", tel);
+    System.out.printf("가입일: %s\n", now);
 
   }
 
