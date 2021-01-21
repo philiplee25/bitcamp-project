39644221ef930a5b9d7f94420b21679379f2c37b
philiplee25 <yplunap25@gmail.com>
2021-01-21 18:02:52 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,17 +1,16 @@
 package com.eomcs.pms;
 
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.BoardHandler2;
+import com.eomcs.pms.handler.BoardHandler3;
+import com.eomcs.pms.handler.BoardHandler4;
+import com.eomcs.pms.handler.BoardHandler5;
+import com.eomcs.pms.handler.BoardHandler6;
 import com.eomcs.pms.handler.MemberHandler;
 import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
 import com.eomcs.util.Prompt;
 
-// 1) Prompt 클래스를 별도의 패키지로 분류한다
-// - com.eomcs.util 패키지 생성
-// - Prompt 클래스를 이 패키지로 옮긴다.
-// - Prompt 클래스를 다른 패키지의 클래스가 사용할 수 있도록 public 으로 공개한다.
-// - Prompt 메서드를 다른 패키지의 클래스가 사용할 수 있도록 public 으로 공개한다.
-// 2) 핸들러 클래스들을 별도의 패키지로 분류한다
-// - com.eomcs.pms.handler
 public class App {
 
   public static void main(String[] args) {
@@ -38,6 +37,30 @@ public class App {
         case "/task/list":
           TaskHandler.list();
           break;
+        case "/board/add":
+          BoardHandler.add();
+        case "/board/list":
+          BoardHandler.list();
+        case "/board2/add":
+          BoardHandler2.add();
+        case "/board2/list":
+          BoardHandler2.list();
+        case "/board3/add":
+          BoardHandler3.add();
+        case "/board3/list":
+          BoardHandler3.list();
+        case "/board4/add":
+          BoardHandler4.add();
+        case "/board4/list":
+          BoardHandler4.list();
+        case "/board5/add":
+          BoardHandler5.add();
+        case "/board5/list":
+          BoardHandler5.list();
+        case "/board6/add":
+          BoardHandler6.add();
+        case "/board6/list":
+          BoardHandler6.list();
         case "quit":
         case "exit":
           System.out.println("안녕!");
---
+++ app/src/main/java/com/eomcs/pms/handler/BoardHandler2.java
@@ -0,0 +1,48 @@
+package com.eomcs.pms.handler;
+
+import com.eomcs.pms.domain.Board;
+import com.eomcs.util.Prompt;
+
+public class BoardHandler2 {
+
+  // 회원 정보를 저장할 메모리의 설계도
+  // - 각 항목의 데이터를 저장할 변수를 선언한다.
+  // - 이 변수를 "필드(field)"라고 부른다.
+
+  static final int LENGTH = 100;
+  static Board[] boards = new Board[LENGTH];  // 레퍼런스 배열 준비  
+  static int size = 0;
+
+  public static void add() {
+    System.out.println("[회원 등록]");
+
+    // 1) 회원 정보를 담을 메모리를 준비한다.
+    Board b = new Board();
+
+    // 2) 사용자가 입력한 값을 Member 인스턴스에 저장한다.
+    b.no = Prompt.inputInt("번호? ");
+    b.title = Prompt.inputString("제목? ");
+    b.content = Prompt.inputString("내용? ");
+    b.name = Prompt.inputString("작성자? ");
+    b.registeredDate = new java.sql.Date(System.currentTimeMillis());
+
+    // 3) 사용자의 정보가 저장된 인스턴스 주소를 레퍼런스 배열에 보관한다.
+    boards[size++] = b;
+    // 위 문장은 컴파일할 때 다음 문장으로 변경된다.
+    //    int temp = size;
+    //    size++;
+    //    members[temp] = m;
+
+  }
+
+  public static void list() {
+    System.out.println("[회원 목록]");
+
+    for (int i = 0; i < size; i++) {
+      Board m = boards[i];
+      // 번호, 제목, 등록일, 작성자, 조회수, 좋아요
+      System.out.printf("%d, %s, %s, %s, %d, %d\n", // 출력 형식 지정
+          m.no, m.title, m.registeredDate, m.name, m.viewCount, m.like);
+    }
+  }
+}
